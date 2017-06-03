47a74eb53c4c324ceafa6b2b2375f6afad435381
mbans <martinbans@gmail.com>
2017-06-03 08:40:54 +0800

    Add the external router IP address as the device controller (now have port forwarding set up).
    Need to make this configurable

--- lumarhome/start-unix.sh
+++ lumarhome/start-unix.sh
@@ -1,4 +1,2 @@
 pkill node
-pkill nodemom
-cd /home/pi/apps/lumarhome/lumarhome
-nodemon app.js > ../../logs/lumarhome.log
+forever start -o ../../logs/lumarhome.log -e ../../logs/lumarhome.err /home/pi/apps/lumarhome/lumarhome/app.js
